e072ed1911e6bd9f81e0e6861c435d930962eca7
LOCALBUONNY\francisco.tadeu <Fr@ncisco082009>
2021-03-21 16:59:02 -0300

    person

---
+++ curso_netcore_5/03_curso_netcore_5_person/curso_netcore_5/curso_netcore_5/Model/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace curso_netcore_5.Model
+{
+    public class Person
+    {
+        public long id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Adress { get; set; }
+        public string gender { get; set; }
+
+    }
+}
