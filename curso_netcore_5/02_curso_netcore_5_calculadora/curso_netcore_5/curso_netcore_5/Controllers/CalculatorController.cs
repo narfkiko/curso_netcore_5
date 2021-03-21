bb218afb9c1eface0dbccb19eeac23995d93f9ce
LOCALBUONNY\francisco.tadeu <Fr@ncisco082009>
2021-03-21 14:06:30 -0300

    calculatora

---
+++ curso_netcore_5/02_curso_netcore_5_calculadora/curso_netcore_5/curso_netcore_5/Controllers/CalculatorController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace curso_netcore_5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{seconfNumber}") ]
+        public IActionResult Get(string firstNumber, string seconfNumber)
+        {
+
+            if (IsNumeric(firstNumber) && IsNumeric(seconfNumber))
+            {
+
+                var sum = ConverttoDecimal(firstNumber) + ConverttoDecimal(seconfNumber);
+                return Ok(sum.ToString());
+
+            }
+
+
+            return BadRequest("inalid input");
+
+        }
+
+        private bool IsNumeric(string strfNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(
+                strfNumber,
+                System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out number);
+            return isNumber;
+        }
+        private decimal ConverttoDecimal(string strfNumber)
+        {
+            decimal decimalvalue;
+            if (decimal.TryParse(strfNumber, out decimalvalue))
+            {
+                return decimalvalue;
+            }
+            return 0;
+        }
+
+    }
+}
